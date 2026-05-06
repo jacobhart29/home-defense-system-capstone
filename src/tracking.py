815260b52ce3ee644705774e2de23ae2ff8732bb
David Hummel <HummelD29@mcvts.net>
2026-05-06 14:41:53 -0400

    Update tracking.py

--- src/tracking.py
+++ src/tracking.py
@@ -1,1 +1,22 @@
-just were all of the tracking goes.
+import cv2
+import numpy as np
+
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while True: 
+    (ret, frame) = cap.read()
+    
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+        cv2.imshow('frame', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+
