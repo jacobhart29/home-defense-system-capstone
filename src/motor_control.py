7f0ba941462d641aaeff1fad5173fb62eb33b019
jacobhart29 <hartj29@mcmsnj.net>
2026-05-07 10:15:46 -0400

    Update config file path for motor control

--- src/motor_control.py
+++ src/motor_control.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 import json
 
-with open('config.json', 'r', encoding='utf-8') as config:
+with open('../config/config.json', 'r', encoding='utf-8') as config:
   fly_wheel_speed = config["motor_speeds"]["fly_wheel"]
   pan_servo_speed = config["motor_speeds"]["pan_servo"]
   tilt_servo_speed = config["motor_speeds"]["tilt_servo"]
