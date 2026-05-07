9aa98771d7f6a1546afed4d2b7cfb5fc4ae55ef7
jacobhart29 <hartj29@mcmsnj.net>
2026-05-07 14:55:40 -0400

    motor control

--- src/motor_control.py
+++ src/motor_control.py
@@ -1,7 +1,11 @@
-import RPi.GPIO as GPIO
+from gpiozero import PWMOutputDevice, OutputDevice
 import time
 import json
 from pathlib import Path
+
+motor_foward = OutputDevice(17)
+motor_backward = OutputDevice(27)
+motor_speed_controller = PWMOutputDevice(13)
 
 config_path = Path(__file__).resolve().parent.parent / 'config' / 'config.json'
 with config_path.open('r', encoding='utf-8') as config_file:
@@ -10,6 +14,32 @@ with config_path.open('r', encoding='utf-8') as config_file:
     pan_servo_speed = config["motor_speeds"]["pan_servo"]
     tilt_servo_speed = config["motor_speeds"]["tilt_servo"]
 
-    fly_wheel_pin = config["pinouts"]["fly_wheel"]
     pan_servo_pin = config["pinouts"]["pan_servo"]
     tilt_servo_pin = config["pinouts"]["tilt_servo"]
+
+
+def stop_fly_wheel_coast():
+    motor_foward.off()
+    motor_backward.off()
+    motor_speed_controller.value = 0
+
+def stop_fly_wheel_brake():
+    motor_foward.on()
+    motor_backward.on()
+    motor_speed_controller.value = 0
+
+def set_fly_wheel(speed, direction):
+    if direction == 1:
+        motor_foward.on()
+        motor_backward.off()
+    elif direction == 0:
+        motor_foward.off()
+        motor_backward.on()
+    else:
+        raise ValueError("Direction must be 0 (backward) or 1 (forward)")
+    motor_speed_controller.value = speed
+
+
+set_fly_wheel(fly_wheel_speed, 1)
+time.sleep(5)
+stop_fly_wheel_brake()
