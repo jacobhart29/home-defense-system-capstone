bcede382baa3302d566e3729b90d7dda16062b67
jacobhart29 <hartj29@mcmsnj.net>
2026-05-07 10:14:01 -0400

    Load motor configuration from config.json
    Added configuration loading for motor speeds and pinouts.

--- src/motor_control.py
+++ src/motor_control.py
@@ -1,1 +1,12 @@
 import RPi.GPIO as GPIO
+import time
+import json
+
+with open('config.json', 'r', encoding='utf-8') as config:
+  fly_wheel_speed = config["motor_speeds"]["fly_wheel"]
+  pan_servo_speed = config["motor_speeds"]["pan_servo"]
+  tilt_servo_speed = config["motor_speeds"]["tilt_servo"]
+
+  fly_wheel_pin = config["pinouts"]["fly_wheel"]
+  pan_servo_pin = config["pinouts"]["pan_servo"]
+  tilt_servo_pin = config["pinouts"]["tilt_servo"]
