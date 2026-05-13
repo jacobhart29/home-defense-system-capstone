48a439f05f0bdfa24b465073d34376faa20847cd
jacobhart29 <hartj29@mcmsnj.net>
2026-05-13 10:04:58 -0400

    Implement JSON save and load for social credits
    Add functionality to save and load social credits from a JSON file.

--- src/social_credit.py
+++ src/social_credit.py
@@ -1,2 +1,10 @@
 import time
 import json
+
+with open("social_cred.json", "w") as f:
+  json.dump(scores, f)
+
+with open("social_cred.json", "r") as f:
+  scores = json.load(f)
+
+print("SOCIAL CREDITS LOADED")
