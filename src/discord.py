816e824ba37140ae71d75bf4214aa40d3419d785
hummeld29 <hummeld29@mcms.com>
2026-05-21 14:51:24 -0400

    bot

--- src/discord.py
+++ src/discord.py
@@ -1,21 +1,30 @@
 
 import discord
+from discord.ext import commands
+import os
+import logging 
+from dotenv import load_dotenv
 
+
+token = ('ODU0ML1NDM3MjODU0MDI1ODQ1NDM3MjM1MjEw.GidQ6a.dOWcYLi3pQ1JoBtJ__zyaQrp8Z5u4y7K10rhIg')
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We ready to go in {bot.user}')
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run('ODU0ML1NDM3MjODU0MDI1ODQ1NDM3MjM1MjEw.GidQ6a.dOWcYLi3pQ1JoBtJ__zyaQrp8Z5u4y7K10rhIg')
+bot.run(token, log_handler=handler, log_level=logging.DEBUG) 
