3a2419325abd8550088b67cff221cbab0476f37d
hummeld29 <hummeld29@mcms.com>
2026-05-21 15:18:32 -0400

    elim

--- src/social_credit.py
+++ src/social_credit.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from time import time
 
 SCORES_PATH = Path(__file__).resolve().parent.parent / 'config' / 'social_credit.json'
 
@@ -25,3 +26,20 @@ def add(user_id, amount):
 def remove(user_id, amount):
     scores[user_id] = scores.get(user_id, 0) - amount
     save()
+
+
+load()
+
+while True:
+    if not SCORES_PATH.exists():
+        print(f"{SCORES_PATH} was deleted. Recreating...")
+        save
+    if scores is None:
+        print("Scores no exist not real person bad for the chinese government")
+        eliminate.murder(2_3_years_dagestan)
+    if scores <= 0:
+        print("bad for the chinese government")
+        eliminate.murder(2_3_years_dagestan)
+    if scores >= 1:
+        print("good for the chinese government")
+        save()
