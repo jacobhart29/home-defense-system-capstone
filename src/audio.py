49342e1abea453e3b4f48aa44848824d5d80af35
jacobhart29 <hartj29@mcmsnj.net>
2026-05-29 11:49:12 -0400

    Implement sound loading and playback in audio.py
    Added sound loading functionality using Pygame mixer.

--- src/audio.py
+++ src/audio.py
@@ -1,2 +1,30 @@
-import audio
+import time
+import pygame
+import os
 
+# SOUND MIXER
+pygame.mixer.init()
+
+SOUND_FOLDER = "./sounds" # has to be changed to the path thingy
+SUPPORTED_FORMATS = (".wav", ".mp3", ".ogg")
+
+LOADED_SOUNDS = {}
+
+if os.path.exists(SOUND_FOLDER):
+  for filename in os.listdir(SOUND_FOLDER):
+    if filename.lower().endswith(SUPPORTED_FORMATS):
+      FILE_PATH = os.path.join(SOUND_FOLDER, filename)
+      try:
+        LOADED_SOUNDS[filename] = pygame.mixer.Sound(FILE_PATH)
+        print(f"LOADED: {filename}")
+      except Exception es e:
+        print(f"ERROR LOADING: {filename}")
+else:
+    print("FOLDER NOT FOUND")
+
+print(f"\nSOUNDS LOADED: {len(LOADED_SOUNDS)}")
+
+
+def play_sound(file_name):
+  if file_name in LOADED_SOUNDS:
+    print(f"PLAYING {file_name})
